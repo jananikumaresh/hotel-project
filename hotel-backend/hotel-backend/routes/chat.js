c6ff383eea5247e1e7dadd2fe6fe8707d9b2dae1
Janani Kumaresh <230701122@rajalakshmi.edu.in>
2026-04-29 09:10:51 +0530

    Delete hotel-backend/hotel-backend directory

--- hotel-backend/hotel-backend/routes/chat.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { chatWithAssistant } = require("../controllers/chatController");
-
-router.post("/", chatWithAssistant);
-
-module.exports = router;
--- hotel-backend/hotel-backend/routes/hotels.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllHotels, getHotelById, createHotel } = require("../controllers/hotelController");
-
-router.get("/", getAllHotels);
-router.get("/:id", getHotelById);
-router.post("/", createHotel);
-
-module.exports = router;
